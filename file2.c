c3796cb44b79ac70905a00a964e868e1b135d147
Gemimah <149108553+Gemimah@users.noreply.github.com>
2023-10-27 07:23:09 +0200

    Add files via upload

--- file2.c
+++ file2.c
@@ -8,9 +8,9 @@ int main() {
     int result2 = (a > b) || (b > c);
     int result3 = !(a == b);
 
-    printf("Result 1: (a > b) && (b < c) = %d\n", result1);
-    printf("Result 2: (a > b) || (b > c) = %d\n", result2);
-    printf("Result 3: !(a == b) = %d\n", result3);
+    printf("Result 1: (a > b) && (b < c) = %d\n", result1);// the result is 1
+    printf("Result 2: (a > b) || (b > c) = %d\n", result2);// the result is 1
+    printf("Result 3: !(a == b) = %d\n", result3); // the result is 1
 
     return 0;
 }
